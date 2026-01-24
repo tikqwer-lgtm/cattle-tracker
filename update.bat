a40caaf54afd3c9d5100cc643291b4531faca6fa
Panko <tik.qwer@gmail.com>
2026-01-24 15:45:52 +0300

    123123

--- update.bat
+++ update.bat
@@ -1,46 +1,31 @@
 @echo off
-:: === Скрипт для Windows: работает без иероглифов ===
-
 chcp 866 >nul
-
 echo.
 echo [1] Проверка изменений...
 git status | findstr "modified\|new file" >nul
 if %errorlevel% neq 0 (
+    echo +++ Нет изменений для отправки.
     echo.
-    echo +++ No changes to commit.
-    echo.
-    goto done
+    goto finish
 )
-
 git add .
-
 echo.
-set /p msg=Opucanue (Enter = "Obnovlenie"): 
-if "%msg%"=="" set msg=Obnovlenie
-
-echo.
-echo +++ Co3daiOT kammut: "%msg%"
+set /p msg=Описание (Enter = "Обновление"): 
+if "%msg%"=="" set msg=Обновление
+echo +++ Коммит: "%msg%"
 git commit -m "%msg%"
-
 echo.
-echo [2] Otpravka v GitHub...
+echo [2] Отправка в GitHub...
 git push origin main
-
+echo.
 if %errorlevel%==0 (
-    echo.
-    echo +++ YCNEX!
-    echo.
-    echo    CauT o6novlён:
+    echo +++ УСПЕШНО!
     echo    https://tikqwer-lgtm.github.io/cattle-tracker/
 ) else (
-    echo.
-    echo --- OXU6KA!
-    echo.
-    echo    Internet? Soedinenie? Prava?
+    echo --- ОШИБКА!
+    echo    Проверьте интернет или выполните 'git pull'
 )
-
-:done
+:finish
 echo.
-echo --- Naimite knopky, cTo6bi 3akpblTb...
+echo --- Готово. Нажмите любую клавишу...
 pause >nul
